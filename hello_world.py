86f3bb906a9f18ac0508d60c1e16215b44497cd5
seongjxnkit <j267175@kumoh.ac.kr>
2022-09-23 09:25:17 +0900

    22.09.23. hello_world.py edit

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,10 @@
-def print_hello():
-    print("Hello World")
+def add(n, m):
+    n += m
+    return n
 
 def print_KIT():
     print("K I T")
 
 if __name__ == '__main__':
     print_KIT()
-    print_hello()
+    print(add(1, 10))
